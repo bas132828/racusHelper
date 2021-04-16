21b629e7f8e440051344b653c1086adc9af266fe
Bas <bas1328@mail.ru>
2021-04-16 19:26:55 +0300

    6

--- script.js
+++ script.js
@@ -1,54 +1,54 @@
-'use strict'
-const rate = document.querySelector('.rate');
-const result = document.querySelector('.result');
-const btn = document.querySelector('.btn');
-let val = document.querySelector('.price') ;
+"use strict";
+const rate = document.querySelector(".rate");
+const result = document.querySelector(".result");
+const btn = document.querySelector(".btn");
+let val = document.querySelector(".price");
 let usdPrice;
 
-const renewPage = function() {
-    document.querySelector('.price').value = 0;
-}
+const renewPage = function () {
+  document.querySelector(".price").value = 0;
+};
 
 renewPage();
 const usdValue = async function () {
-    const resp = await fetch("https://www.cbr-xml-daily.ru/daily_json.js");
-    const data = await resp.json();
-    // console.log(data)
-    console.log(data.Valute.USD.Value);
-    usdPrice = data.Valute.USD.Value;
-    rate.textContent = usdPrice;
-    };
+  const resp = await fetch("https://www.cbr-xml-daily.ru/daily_json.js");
+  const data = await resp.json();
+  // console.log(data)
+  console.log(data.Valute.USD.Value);
+  usdPrice = data.Valute.USD.Value;
+  rate.textContent = usdPrice;
+};
 
 usdValue();
 
-const calc = function(e) {
+const calc = function (e) {
+  e.preventDefault;
+  const value = document.querySelector(".price").value;
 
- e.preventDefault;
- const value =document.querySelector('.price').value;
+  if (!Number(val.value)) return alert("ему точно буквы в рубли считать?");
 
- if(!Number(val.value)) return alert ('ему точно буквы в рубли считать?')
- 
- const toPay = Number(usdPrice)*Number(value)
- result.textContent = `${Math.trunc(toPay)} руб. ${Math.trunc(toPay*100)%100} коп.`
-}
+  const toPay = Number(usdPrice) * Number(value);
+  result.textContent = `${Math.trunc(toPay)} руб. ${
+    Math.trunc(toPay * 100) % 100
+  } коп.`;
+};
 
-btn.addEventListener('click', calc)
-
+btn.addEventListener("click", calc);
 
 //copy to clipboard feature
 
-const copyTextareaBtn = document.querySelector('.js-textareacopybtn');
+const copyTextareaBtn = document.querySelector(".js-textareacopybtn");
 
-copyTextareaBtn.addEventListener('click', function(event) {
-  const copyTextarea = document.querySelector('.js-copytextarea');
+copyTextareaBtn.addEventListener("click", function (event) {
+  const copyTextarea = document.querySelector(".js-copytextarea");
   copyTextarea.focus();
   copyTextarea.select();
 
   try {
-    const successful = document.execCommand('copy');
-    const msg = successful ? 'successful' : 'unsuccessful';
-    console.log('Copying text command was ' + msg);
+    const successful = document.execCommand("copy");
+    const msg = successful ? "successful" : "unsuccessful";
+    console.log("Copying text command was " + msg);
   } catch (err) {
-    console.log('Oops, unable to copy');
+    console.log("Oops, unable to copy");
   }
 });
