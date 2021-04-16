43a37a24a06dfce0b714cae4deba0b63720514eb
Andrei Bodrov <63089699+bas132828@users.noreply.github.com>
2021-04-16 13:40:43 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,37 @@
+'use strict'
+const rate = document.querySelector('.rate');
+
+const result = document.querySelector('.result');
+const btn = document.querySelector('.btn');
+let val = document.querySelector('.price') ;
+let usdPrice;
+
+
+const renewPage = function() {
+    document.querySelector('.price').value = 0;
+}
+
+
+renewPage();
+
+const usdValue = async function () {
+    const resp = await fetch("https://www.cbr-xml-daily.ru/daily_json.js");
+    const data = await resp.json();
+    console.log(data)
+    console.log(data.Valute.USD.Value);
+    usdPrice = data.Valute.USD.Value;
+    rate.textContent = usdPrice;
+    };
+
+usdValue();
+
+
+const calc = function(e) {
+ e.preventDefault;
+ const value =document.querySelector('.price').value;
+ const toPay = Number(usdPrice)*Number(value)
+ result.textContent = toPay.toFixed(2)
+
+}
+
+btn.addEventListener('click', calc)
